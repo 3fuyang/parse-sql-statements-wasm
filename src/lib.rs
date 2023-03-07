ff71b84efae9a59dfaefae1204ffd24cbe0a693f
3fuyang <fuyang@matrixorigin.cn>
2023-03-07 11:01:03 +0800

    chore: remove unnecessary js imports

--- src/lib.rs
+++ src/lib.rs
@@ -5,30 +5,26 @@ use sqlparser::dialect::MySqlDialect;
 use sqlparser::parser::Parser;
 
 #[wasm_bindgen]
-extern {
-    // Send serialized result to the client. 
-    pub fn post_client(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn parse_statements(sql: &str) {
+pub fn parse_statements(sql: &str) -> String {
     let dialect = MySqlDialect {};
     let try_result = Parser::new(&dialect).try_with_sql(sql);
     let mut tokenized_parser = match try_result {
-        Ok(parser) => parser,
+        Ok(parser) =>  parser,
         Err(err) => {
-            return post_client(&to_string(&(err.to_string())).unwrap());
+            let serialized_err = to_string(&(err.to_string())).unwrap();
+            return serialized_err;
         },
     };
     let statements_result = tokenized_parser.parse_statements();
     let statements = match statements_result {
         Ok(s) => s,
         Err(err) => {
-            return post_client(&to_string(&(err.to_string())).unwrap());
+            let serialized_err = to_string(&(err.to_string())).unwrap();
+            return serialized_err;
         }
     };
 
     let serialzed = to_string(&statements).unwrap();
 
-    post_client(&serialzed);
+    return serialzed;
 }
