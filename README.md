af3c9210dbc1b46ffd8517dfcd457504bf1356ee
fwio <624835339@qq.com>
2023-03-11 11:26:28 +0800

    chore: returns struct for better usability

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 This is a `wasm` wrapper of [sqlparser](https://crates.io/crates/sqlparser), exposing the `parse_statements` method only.
 
+## Method
+
+```ts
+interface ParseResult {
+  /** Indicates whether or not the given sql is valid. */
+  success: boolean
+  /** You may unserialize this using `JSON.parse()`. */
+  serialzed_result: string
+}
+
+export function parse_statements(text: string): ParseResult
+```
+
 ## Build
 
 [wasm-pack](https://github.com/rustwasm/wasm-pack) required.
--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,28 @@ use serde_json::to_string;
 use sqlparser::dialect::MySqlDialect;
 use sqlparser::parser::Parser;
 
+/// Struct of the parse result.
+#[wasm_bindgen(getter_with_clone)]
+pub struct ParseResult {
+    /// Indicates whether the passed-in sql is valid or not.
+    pub success: bool,
+    /// The serialzed JSON when succeeded, an error message when failed.
+    pub serialized_result: String,
+}
+
+/// Parse potentially multiple `sql` statements.
 #[wasm_bindgen]
-pub fn parse_statements(sql: &str) -> String {
+pub fn parse_statements(sql: &str) -> ParseResult {
     let dialect = MySqlDialect {};
     let try_result = Parser::new(&dialect).try_with_sql(sql);
     let mut tokenized_parser = match try_result {
         Ok(parser) =>  parser,
         Err(err) => {
             let serialized_err = to_string(&(err.to_string())).unwrap();
-            return serialized_err;
+            return ParseResult {
+                success: false,
+                serialized_result: serialized_err,
+            };
         },
     };
     let statements_result = tokenized_parser.parse_statements();
@@ -20,11 +33,17 @@ pub fn parse_statements(sql: &str) -> String {
         Ok(s) => s,
         Err(err) => {
             let serialized_err = to_string(&(err.to_string())).unwrap();
-            return serialized_err;
+            return ParseResult {
+                success: false,
+                serialized_result: serialized_err,
+            };
         }
     };
 
     let serialzed = to_string(&statements).unwrap();
 
-    return serialzed;
+    ParseResult {
+        success: true,
+        serialized_result: serialzed,
+    }
 }
